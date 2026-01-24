735518d18e1be8a677ded382dc465dae457980bc
Ganraj Dol <84555910+ganrajdol99215@users.noreply.github.com>
2026-01-24 16:39:08 +0900

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
 
-  host: "finance-rds.cq72oge0kzty.us-east-1.rds.amazonaws.com",
+  host: "finance.cq72oge0kzty.us-east-1.rds.amazonaws.com",
   user: "ganraj",
   password: "9921569869",
   database: "finance",
